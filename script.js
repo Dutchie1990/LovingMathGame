f8838d01b306ea857f7c7a73eba49224dcc1578d
Dutchie1990 <danielboone1990@gmail.com>
2021-01-06 18:42:45 +0000

    Add the first code structure and setting up eventlisteners

--- script.js
+++ script.js
@@ -1,35 +1,53 @@
-function runGame(){
+// wait for the DOM to finish loading before running the game 
+// Get the button element and add event listeners to them
+
+document.addEventListener("DOMContentLoaded", function () {
+    let buttons = document.getElementsByTagName("button")
+
+    for (let button of buttons) {
+        button.addEventListener("click", function () {
+            if (this.getAttribute("data-type") == "submit") {
+                alert("You Clicked Submit")
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked: ${gameType}`)
+            }
+        })
+    }
+})
+
+function runGame() {
 
 }
 
-function checkAnswer(){
+function checkAnswer() {
 
 }
 
-function calculateCorrectAnswer(){
+function calculateCorrectAnswer() {
 
 }
 
-function incrementScore(){
+function incrementScore() {
 
 }
 
-function incrementWrongAnswer(){
+function incrementWrongAnswer() {
 
 }
 
-function displayAdditionsQuestion(){
+function displayAdditionsQuestion() {
 
 }
 
-function displaySubtractQuestion(){
-    
+function displaySubtractQuestion() {
+
 }
 
-function displayMultiplyQuestion(){
-    
+function displayMultiplyQuestion() {
+
 }
 
-function displayDivideQuestion(){
-    
+function displayDivideQuestion() {
+
 }
